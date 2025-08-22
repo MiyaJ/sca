901718fabf1c0f773979f32c90e2acde2acf83c1
CaiXiaowei <540110865@qq.com>
2025-08-22 15:40:57 +0800

    Initial commit

---
+++ sca-admin/src/test/java/site/corki/admin/ScaAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package site.corki.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
